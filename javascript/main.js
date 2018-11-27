b99ada392f8ef3a7a598089aa2271aff00fe2e5b
Miriam Rozenbaum <miriamrozenbaum@miriams-mbp.attlocal.net>
2018-11-27 18:07:44 -0500

    redid the test with psd files provided

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,28 @@
 
 $(document).ready(function () {
-  // Rotation for background images of website
-  $('.carousel').carousel({
-  interval: 8000
-  });
-  // Validation for required fields in Booking Form
-  $("input").attr('required',true);
-  $("select").attr('required',true);
+  // background images 
+  var images = [
+  "images/Images%20Slides/MainIMage/Layer%2041.png",
+  "images/Images%20Slides/MainIMage/shutterstock_140369722.png",
+  "images/Images%20Slides/MainIMage/shutterstock_146450198.png",
+  "images/Images%20Slides/MainIMage/shutterstock_163701617.png"
+  ]
+  // background images div in html
+  var imageHead = document.getElementById("image-head");
+  var i = 0;
+  setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+  }, 8000);
+
+
+  $('#datepicker').datepicker({
+      uiLibrary: 'bootstrap4'
+  });  
+  
   
 });
 
